1309708384066d89cff06482e00805fab89cb87c
rmxhaha <code000123@gmail.com>
2016-04-23 18:17:33 +0700

    adding non working spiral bullet

---
+++ src/Model/BulletType/SpiralBullet.java
@@ -0,0 +1,74 @@
+package Model.BulletType;
+
+import Model.Bullet;
+import Model.Organism;
+import Model.Universe;
+
+/**
+ * Created by anthony on 4/22/16.
+ * Updated by candra on 4/23/16.
+ */
+public class SpiralBullet extends Bullet {
+    private float movementAngle; // in radian
+    private final float movementSpeed;
+    private final float movementAngleSpeed;
+    /**
+     * SpiralBullet Constructor
+     *
+     * @param universe   Universe it belong in
+     *                   \return Animal
+     * @param x          x location
+     * @param y          y location
+     * @param currentAge current age
+     * @param startingMovementAngle bullet angle movement direction (in radian)
+     * @param movementSpeed movement speed
+     * @param movementAngleSpeed angle trajactory changes in the bullet
+     */
+    public SpiralBullet(Universe universe, int x, int y, float currentAge, float startingMovementAngle, float movementSpeed, float movementAngleSpeed) {
+        super(universe, x, y, currentAge);
+        this.movementAngle = startingMovementAngle;
+        this.movementSpeed = movementSpeed;
+        this.movementAngleSpeed = movementAngleSpeed;
+        setNextLocation();
+
+    }
+
+    @Override
+    protected void update_logic() {
+        super.update_logic();
+        float dt = 1000 / speed();
+        movementAngle += dt * movementAngleSpeed;
+    }
+    
+    @Override
+    protected final void setNextLocation(){
+        targetX = x + new Double(8*Math.sin(movementAngle)).intValue();
+        targetY = y + new Double(8*Math.cos(movementAngle)).intValue();
+    }
+
+    @Override
+    public float speed() {
+        return movementSpeed;
+    }
+
+
+    @Override
+    public char ch() {
+        return 'S';
+    }
+
+    @Override
+    public int age() {
+        return 0;
+    }
+
+    @Override
+    public int power() {
+        return 0;
+    }
+
+    @Override
+    public void interact(Organism o) {
+
+    }
+}
