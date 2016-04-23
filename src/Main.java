b48ce26910e548d5a36268f154226c644a388680
rmxhaha <code000123@gmail.com>
2016-04-23 18:16:41 +0700

    update main to show straight bullet

--- src/Main.java
+++ src/Main.java
@@ -2,32 +2,39 @@ import Factory.OrganismFactory;
 import Model.Board;
 import Model.Organism;
 import Model.Universe;
+import Model.BulletType.*;
+import Model.Creature.*;
+
+
 
 import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args){
 		int w,h,N;
-		Scanner scanner = new Scanner(System.in);
-		w = scanner.nextInt();
-		h = scanner.nextInt();
-		N = scanner.nextInt();
+		w = 20;
+		h = 20;
+		N = 3;
 		
 		try {
 			Board b = new Board(w,h);
 			Universe u = new Universe(b,N);
-			OrganismFactory organismFactory = new OrganismFactory(u,0.f);
-			Organism eagle =  organismFactory.CreateEagle(0,0);
-			Organism human = organismFactory.CreateHuman(1,1);
-			
+			OrganismFactory factory = new OrganismFactory(u,0);
+                        Organism m = new StraightBullet(u,10,10,0,2,2);
+                        Organism m2 = factory.CreateDeer(0, 0);
+
+                        u.add(m);
+                        u.add(m2);
+                        
 			while( true ){
 				try {
-					Thread.sleep(100);
+                                    Thread.sleep(1000);
 				}
 				catch(Exception e){
 				}
 				u.board.PrintBoard();
-				u.update(100);
+//                                System.out.println(m.getX() + " " + m.getY());
+				u.update(1000);
 			}
 		}
 		catch(Exception e){
