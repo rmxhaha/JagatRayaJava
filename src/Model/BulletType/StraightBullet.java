24b4ff38c0775cbd541eb59392526c2b4a3cf841
rmxhaha <code000123@gmail.com>
2016-04-23 17:26:09 +0700

    Adding formula for straight bullet movement

--- src/Model/BulletType/StraightBullet.java
+++ src/Model/BulletType/StraightBullet.java
@@ -3,11 +3,15 @@ package Model.BulletType;
 import Model.Bullet;
 import Model.Organism;
 import Model.Universe;
+import Utility.IntPair;
 
 /**
  * Created by anthony on 4/22/16.
+ * Updated by candra on 4/23/16.
  */
 public class StraightBullet extends Bullet {
+    private float movementAngle; // in radian
+    private float movementSpeed;
     /**
      * Bullet Constructor
      * <p/>
@@ -17,20 +21,26 @@ public class StraightBullet extends Bullet {
      * @param x          x location
      * @param y          y location
      * @param currentAge current age
+     * @param movementAngle bullet angle movement direction (in radian)
      */
-    public StraightBullet(Universe universe, int x, int y, float currentAge) {
+    public StraightBullet(Universe universe, int x, int y, float currentAge, float movementAngle, float movementSpeed) {
         super(universe, x, y, currentAge);
+        this.movementAngle = movementAngle;
+        this.movementSpeed = movementSpeed;
+    }
+
+    
+    @Override
+    protected void setNextLocation(){
+        targetX = x + new Double(8*Math.sin(movementAngle)).intValue();
+        targetY = y + new Double(8*Math.cos(movementAngle)).intValue();
     }
 
     @Override
     public float speed() {
-        return 0;
+        return movementSpeed;
     }
 
-    @Override
-    protected void update_logic() {
-
-    }
 
     @Override
     public char ch() {
