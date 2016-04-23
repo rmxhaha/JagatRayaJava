87d1277666f5618d9769cd9031996b01f9914e31
rmxhaha <code000123@gmail.com>
2016-04-23 17:25:15 +0700

    adding health to boss

--- src/Model/Boss.java
+++ src/Model/Boss.java
@@ -2,8 +2,10 @@ package Model;
 
 /**
  * Created by anthony on 4/22/16.
+ * Updated by candra on 4/23/16.
  */
 public class Boss extends Animal {
+	private float health;
     /**
      * Boss Constructor
      * <p/>
@@ -14,8 +16,9 @@ public class Boss extends Animal {
      * @param y          y location
      * @param currentAge current age
      */
-    public Boss(Universe universe, int x, int y, float currentAge) {
+    public Boss(Universe universe, int x, int y, float currentAge, float chealth) {
         super(universe, x, y, currentAge);
+		health = chealth;
     }
 
     @Override
@@ -42,9 +45,17 @@ public class Boss extends Animal {
     public int power() {
         return 0;
     }
+	
+	public float getHealth(){
+		return health;
+	}
 
     @Override
     public void interact(Organism o) {
-
+		if( o instanceof Bullet ){
+			health -= o.power();
+			if( health < 0 )
+				forceKill();
+		}
     }
 }
